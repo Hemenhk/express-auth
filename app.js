29b2430b04363db78e9ca21d57fb999cd42a14c7
Hemenhk <hemen.hiwakamal@hotmail.com>
2023-03-04 16:31:20 +0000

    Create login route

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const User = require("./model/user");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("./middleware/auth");
 
 const app = express();
 
@@ -50,8 +51,43 @@ app.post("/register", async (req, res) => {
     }
 });
 
-app.post("/login", (req, res) => {
+app.post("/login", async (req, res) => {
+    try {
+        const {email, password} = req.body;
 
+        // validate user input
+        if (!(email, password)) {
+            res.status(400).send("All input is required")
+        }
+
+        // validate if user exists in the db
+        const user = await User.findOne({email});
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            // create token
+            const token = jwt.sign(
+                {user_id: user._id, email},
+                ` ${process.env.TOKEN_KEY}`,
+                {
+                    expiresIn: "2h",
+                }
+            );
+
+            // save user token
+            user.token = token;
+
+            // user
+            res.status(200).json(user);
+        }
+        res.status(400).send("Invalid Credentials");
+
+    } catch (err) {
+        console.log(err)
+    }
+});
+
+app.post("/welcome", auth, (req, res) => {
+    res.status(200).send("Welcome!")
 });
 
 
